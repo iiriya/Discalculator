f1bc8a32dcee9d94a77eb41f9f6fde00b1cbdbff
Lucio Benini <iiriya@iiriya.com>
2016-02-20 16:07:56 +0100

    Added files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Lucio Benini">
+//      Copyright (c) Lucio Benini. All rights reserved.
+// </copyright>
+// <author>Lucio Benini</author>
+// <summary>Contains assembly information.</summary>
+// <remarks>
+//      Iiriya's Discalculator.
+//      
+//      This project contains a discount calculator.
+//      
+//      This solution and all its components its owned by Lucio Benini and his companies.
+//      This solution is designed in C# for Microsoft .NET Framework and works only under the Windows Operative System.
+//      For Mono or other platforms integrations contact the main project owner.
+//      The designer and his companies aren't responsible for any damage due to unauthorized installations or usages.
+//      
+//      This project is created and designed under Microsoft .NET 2.0 environment and runtime and requires the runtime version 2.0 or later.
+//      
+//      This project is licensed. Copyright © Lucio Benini 2016. All Rights Reserved.
+// </remarks>
+//-----------------------------------------------------------------------------------------------------------------------
+
+#region Using Directives
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security;
+#endregion
+
+#region Application Attributes
+[assembly: AssemblyTitle("Discalculator")]
+[assembly: AssemblyDescription("Discount calculator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Iiriya")]
+[assembly: AssemblyProduct("Discalculator")]
+[assembly: AssemblyCopyright("Copyright © 2016 Lucio Benini")]
+[assembly: AssemblyTrademark("Discalculator")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("74320841-5e02-4780-9b7a-21ac377a0026")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+#endregion
